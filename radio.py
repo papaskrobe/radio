72f48b76c75d325114c116e19c9c0ab9281d6c5f
papaskrobe <bnl083@gmail.com>
2017-12-09 03:32:23 +0000

    Adjust scale for small screen

--- radio.py
+++ radio.py
@@ -1,13 +1,17 @@
 import Tkinter as tk
 import glob
 import id3reader
+import pygame
+
+pygame.mixer.init()
+player = pygame.mixer.music
 
 root = tk.Tk()
 root.geometry("320x240")
 selector = tk.Frame(root)
 selector.grid(row=0, column=0)
 
-bands = tuple(sorted([(i.split('/'))[-1] for i in glob.glob('/home/ryan/Music/*')]))
+bands = tuple(sorted([(i.split('/'))[-1] for i in glob.glob('/home/pi/Music/*')]))
 bnames = tk.StringVar(value=bands)
 
 yScroll = tk.Scrollbar(orient=tk.VERTICAL)
@@ -16,8 +20,7 @@ yScroll.grid(row=0,column=1, sticky=tk.N+tk.S)
 def select_album(evt):
 	bPlay.config(state=tk.NORMAL)
 
-tk.Label(selector, text="Albums:").grid(column=1, row=0)
-lAlbums = tk.Listbox(selector, height=8, exportselection=False)
+lAlbums = tk.Listbox(selector, height=6, exportselection=False)
 lAlbums.grid(column=1, row=1)
 lAlbums.bind('<<ListboxSelect>>', select_album)
 
@@ -25,14 +28,13 @@ def select_band(evt):
 	w = evt.widget
 	index = int(w.curselection()[0])
 	value = w.get(index)
-	albums = tuple([(i.split('/'))[-1] for i in glob.glob('/home/ryan/Music/' + value + '/*')])
+	albums = tuple([(i.split('/'))[-1] for i in glob.glob('/home/pi/Music/' + value + '/*')])
 	lAlbums.delete(0, 100)
 	for i in albums:
 		lAlbums.insert(tk.END, i) #tk.StringVar(value=songs))
 	bPlay.config(state=tk.DISABLED)
 
-tk.Label(selector, text="Bands:").grid(column=0, row=0)
-lBands = tk.Listbox(selector, yscrollcommand=yScroll.set, listvariable=bnames, height=8, selectmode=tk.SINGLE, exportselection=False)
+lBands = tk.Listbox(selector, yscrollcommand=yScroll.set, listvariable=bnames, height=6, selectmode=tk.SINGLE, exportselection=False)
 lBands.bind('<<ListboxSelect>>', select_band)
 lBands.grid(column=0, row=1)
 yScroll['command']=lBands.yview
@@ -42,7 +44,7 @@ def play():
 	bandValue = lBands.get(bandIndex)
 	albumIndex = lAlbums.curselection()[0]
 	albumValue = lAlbums.get(albumIndex)
-	songs = tuple([(i.split('/'))[-1] for i in sorted(glob.glob('/home/ryan/Music/' + bandValue + '/' + albumValue + '/*.mp3'),key=lambda i:int(id3reader.Reader(i).getValue('track').split('/')[0]))])
+	songs = tuple([(i.split('/'))[-1] for i in sorted(glob.glob('/home/pi/Music/' + bandValue + '/' + albumValue + '/*.mp3'),key=lambda i:int(id3reader.Reader(i).getValue('track').split('/')[0]))])
 	for i in songs:
 		print(i)
 
